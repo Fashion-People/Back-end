721a3bd560c0043d6ca694b469ad04f77ab61e09
오현서 <ohs070611@naver.com>
2024-03-13 19:34:10 +0900

    Fix : 주석 처리

--- capston/src/test/java/com/example/capston/CapstonApplicationTests.java
+++ capston/src/test/java/com/example/capston/CapstonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.capston;
+/*package com.example.capston;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class CapstonApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
